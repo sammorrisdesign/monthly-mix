11fbd30ca3cb3b46ee4ace48b06872dc4cebf344
Sam Morris <mail@sammorrisdesign.co.uk>
2019-10-16 13:58:05 -0400

    after x number of tracks show message to subscribe

--- src/js/modules/subscribe.js
+++ src/js/modules/subscribe.js
@@ -21,8 +21,44 @@ const bindings = () => {
     });
 }
 
+const displayMessageAfterTracks = () => {
+    const isUnsubscribed = hasEmailParams();
+    let playCount = 0;
+
+    if (isUnsubscribed) {
+        mediator.subscribe('play', () => {
+            playCount++;
+
+            if (playCount === 5) {
+                document.querySelector('.js-subscribe').click();
+            }
+        });
+    }
+}
+
+const hasEmailParams = () => {
+    const string = window.location.search;
+
+    if (string) {
+        const params = string.replace('?', '').split('&');
+        const values = new Object;
+
+        params.forEach(param => {
+            param = param.split('=');
+            values[param[0]] = param[1];
+        });
+
+        if (values.utm_source && values.utm_source === 'newsletter') {
+            return false;
+        }
+    }
+
+    return true;
+}
+
 export default {
     init: () => {
         bindings();
+        displayMessageAfterTracks();
     }
 }
